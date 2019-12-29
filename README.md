bd2e0cf55993d71e0eb1b5347ca370c4edcb3cee
Huang Jian-Min <yfr.huang@hotmail.com>
2019-12-29 12:03:19 +0800

    update

--- README.md
+++ README.md
@@ -3,8 +3,12 @@ k8s-sample-2020
 
 ```
 mvn compile jib:build \
--Djib.to.auth.username=$USERNAME \
--Djib.to.auth.password=$PASSWORD
+-Djib.to.auth.username=$DOCKER_HUB_USERNAME \
+-Djib.to.auth.password=$DOCKER_HUB_PASSWORD
+```
+
+```
+helm dep up
 ```
 
 ### references
--- helm/create-k3d-helm-istio.sh
+++ helm/create-k3d-helm-istio.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
 k3d create --server-arg --no-deploy --server-arg traefik
+
 sleep 10s
+
 export KUBECONFIG="$(k3d get-kubeconfig --name='k3s-default')"
 helm init
 kubectl create serviceaccount --namespace kube-system tiller
@@ -7,12 +11,3 @@ kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admi
 kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
 istioctl manifest apply --set profile=demo
 kubectl label namespace default istio-injection=enabled
-k3d import-images local/sample-pt-gw:1
-sleep 3s
-k3d import-images local/sample-lg-gw:1
-sleep 3s
-k3d import-images local/sample-task:1
-sleep 3s
-k3d import-images local/sample-member:1
-sleep 3s
-k3d import-images local/sample-item:1
