4bdbb12cbe72c912533ad776fa2886771228d2a2
Huang Jian-Min <yfr.huang@hotmail.com>
2019-12-31 04:56:31 +0800

    add redis mq

--- sample-task/src/main/java/org/yfr/sample/task/SampleTaskApp.java
+++ sample-task/src/main/java/org/yfr/sample/task/SampleTaskApp.java
@@ -3,9 +3,10 @@ package org.yfr.sample.task;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.redis.spring.RedisLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,6 +15,16 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableSchedulerLock(defaultLockAtMostFor = "PT2M")
 public class SampleTaskApp {
+
+    @Bean
+    public Queue memberQueue() {
+        return new Queue("member", false);
+    }
+
+    @RabbitListener(queues = "member")
+    public void listen(String element) {
+        System.out.println("Message read from member queue : " + element);
+    }
 
     @Bean
     public LockProvider lockProvider(final RedisConnectionFactory redisConnectionFactory) {
--- sample-task/src/main/java/org/yfr/sample/task/schedule/SampleTask.java
+++ sample-task/src/main/java/org/yfr/sample/task/schedule/SampleTask.java
@@ -2,15 +2,18 @@ package org.yfr.sample.task.schedule;
 
 import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.core.SchedulerLock;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.yfr.sample.common.entity.Item;
+
+import javax.annotation.Resource;
+import java.util.UUID;
 
 @Slf4j
 @Component
@@ -19,7 +22,20 @@ public class SampleTask {
     @Value("${host.sample.item}")
     private String itemHost;
 
+    @Resource
+    private RabbitTemplate rabbitTemplate;
+
     private RestTemplate restTemplate = new RestTemplate();
+
+    @Scheduled(cron = "*/5 * * * * *")
+    @SchedulerLock(name = "addMember", lockAtMostForString = "PT2M")
+    public void addMember() {
+        try {
+            rabbitTemplate.convertAndSend("member", UUID.randomUUID().toString().replace("-", ""));
+        } catch (Exception ex) {
+            log.error(ex.getMessage(), ex);
+        }
+    }
 
     @Scheduled(cron = "*/5 * * * * *")
     @SchedulerLock(name = "parseItem", lockAtMostForString = "PT2M")
